9bf6738010ea91c57a33922bab7924a614ade28e
SC Projects LK <80474245+scprojectslk@users.noreply.github.com>
2021-09-08 20:19:28 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -90,8 +90,8 @@ async def answer(bot, query):
 def get_reply_markup(query):
     buttons = [
         [
-            InlineKeyboardButton('Search again', switch_inline_query_current_chat=query),
-            InlineKeyboardButton('More Bots', url='https://t.me/subin_works/122')
+            InlineKeyboardButton('sᴇᴀʀᴄʜ ᴀɢᴀɪɴ', switch_inline_query_current_chat=query),
+            InlineKeyboardButton('ᴍᴏʀᴇ ʙᴏᴛs', url='https://t.me/SCProjectsLK')
         ]
         ]
     return InlineKeyboardMarkup(buttons)
